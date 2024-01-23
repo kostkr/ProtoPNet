37c5bf8b90138766eacfef21dc74a6e3ec6e249a
Miroslav Dovger <miroslavdovger@gmail.com>
2024-01-24 00:02:59 +0100

    api

--- src/main/java/com/example/website/controller/RateController.java
+++ src/main/java/com/example/website/controller/RateController.java
@@ -2,6 +2,7 @@ package com.example.website.controller;
 
 import com.example.website.model.ImageReader;
 import com.example.website.model.MyImage;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,15 +11,17 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class RateController {
+    @Autowired
+    ImageReader imageReader;
 
     @GetMapping("/getImages")
     public List<MyImage> getImages() {
-        return ImageReader.readImages();
+        return imageReader.readImages();
     }
 
     @PostMapping("/rateImage")
-    public void rateImage(@RequestBody String rate) {
-        System.out.println("rated " + rate );
+    public void rateImage(@RequestBody String data) {
+        imageReader.showUserAnswer(data);
     }
 
 }
--- src/main/java/com/example/website/model/ImageReader.java
+++ src/main/java/com/example/website/model/ImageReader.java
@@ -1,53 +1,137 @@
 package com.example.website.model;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Base64;
 import java.util.List;
+import java.util.Random;
 
+@Service
 public class ImageReader {
-    public static List<MyImage> readImages() {
-        ArrayList<MyImage> images = new ArrayList<>();
-        try {
-            File file1 = new File("E:\\university_subject\\pz\\webSite\\src\\test\\testImage1.jpg");
-            MyImage myImage1 = new MyImage(1, convertToBase64(ImageIO.read(file1)));
-            images.add(myImage1);
 
-            File file2 = new File("E:\\university_subject\\pz\\webSite\\src\\test\\testImage2.jpg");
-            MyImage myImage2 = new MyImage(2, convertToBase64(ImageIO.read(file2)));
-            images.add(myImage2);
+    private long uniqueImageIndex = 0L;
 
-            File file3 = new File("E:\\university_subject\\pz\\webSite\\src\\test\\testImage2.jpg");
-            MyImage myImage3 = new MyImage(3, convertToBase64(ImageIO.read(file3)));
-            images.add(myImage3);
+    @Value("${script.path}")
+    private String scriptPath;
 
-            File file4 = new File("E:\\university_subject\\pz\\webSite\\src\\test\\testImage2.jpg");
-            MyImage myImage4 = new MyImage(4, convertToBase64(ImageIO.read(file4)));
-            images.add(myImage4);
+    @Value("${model.dir.path}")
+    private String modeldirPath;
 
-            File file5 = new File("E:\\university_subject\\pz\\webSite\\src\\test\\testImage2.jpg");
-            MyImage myImage5 = new MyImage(5, convertToBase64(ImageIO.read(file5)));
-            images.add(myImage5);
+    @Value("${model.name}")
+    private String modelName;
 
-            File file6 = new File("E:\\university_subject\\pz\\webSite\\src\\test\\testImage2.jpg");
-            MyImage myImage6 = new MyImage(6, convertToBase64(ImageIO.read(file6)));
-            images.add(myImage6);
+    @Value("${images.base.path}")
+    private String baseImagesPath;
 
-        } catch (IOException e) {
-            System.err.println("Images unread");
+    public List<MyImage> readImages() {
+        ArrayList<MyImage> images = new ArrayList<>(11);
+        try{
+            // find image to analyze
+            String imgPath = findPathImageToAnalyze();
+
+            // config model script
+            ProcessBuilder processBuilder = new ProcessBuilder(
+                    "python", scriptPath,
+                    "-modeldir", this.modeldirPath, "-model", this.modelName, "-imgpath", imgPath);
+
+            //start model script
+            Process process = processBuilder.start();
+
+            // read images from script
+            InputStream inputStream = process.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            String line;
+            StringBuilder outputBuilder = new StringBuilder();
+            while ((line = reader.readLine()) != null) {
+                outputBuilder.append(line);
+            }
+
+            File file = new File(imgPath);// add original image
+            MyImage myImage = new MyImage(getUniqueImageIndex(), convertToBase64(ImageIO.read(file)));
+            images.add(myImage);
+
+            long index = 1;// add predicted images
+            for (String image : outputBuilder.toString().split(", ")) {
+                images.add(new MyImage(index, image));
+                ++index;
+            }
+
+            int exitCode = process.waitFor();
+            //System.out.println("ProtoPNet script exited with code: " + exitCode);
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
         }
 
         return images;
     }
 
-    private static String convertToBase64(BufferedImage image) throws IOException {
+    public String findPathImageToAnalyze(){
+        File randomFolder = getRandomFolder(baseImagesPath);
+
+        if (randomFolder != null) {
+            File randomImage = getRandomImage(randomFolder);
+            if (randomImage != null) {
+                return randomImage.getPath();
+            } else {
+                System.err.println("There are no images in the selected folder");
+            }
+        } else {
+            System.err.println("There are no folders at the specified path");
+        }
+
+        return "";
+    }
+
+    public void showUserAnswer(String data){
+        System.out.println(data);
+    }
+
+    private File getRandomFolder(String basePath) {
+        File baseDir = new File(basePath);
+        File[] folders = baseDir.listFiles(File::isDirectory);
+
+        if (folders != null && folders.length > 0) {
+            Random random = new Random();
+            int randomIndex = random.nextInt(folders.length);
+            return folders[randomIndex];
+        }
+
+        return null;
+    }
+
+    private File getRandomImage(File folder) {
+        File[] images = folder.listFiles((dir, name) -> name.toLowerCase().endsWith(".jpg") || name.toLowerCase().endsWith(".jpeg"));
+
+        if (images != null && images.length > 0) {
+            Random random = new Random();
+            int randomIndex = random.nextInt(images.length);
+            return images[randomIndex];
+        }
+        return null;
+    }
+
+    private String convertToBase64(BufferedImage image) throws IOException {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         ImageIO.write(image, "jpg", baos);
         byte[] imageBytes = baos.toByteArray();
         return Base64.getEncoder().encodeToString(imageBytes);
+    }
+
+    private long getUniqueImageIndex(){
+        increaseUniqueIndex();
+        return uniqueImageIndex == 0 ? 0 : uniqueImageIndex - 1;
+    }
+
+    private void increaseUniqueIndex(){
+        if(uniqueImageIndex < Long.MAX_VALUE)
+            uniqueImageIndex += 1;
+        else
+            uniqueImageIndex = 0;
     }
 }
